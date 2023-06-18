30e2e37d532b777740a11d691ce4a20b7c7a1d14
rakhazufar <rakhazufar1@gmail.com>
2023-06-18 14:21:52 +0700

    add prompt card and function to copy to the clipboard

---
+++ app/profile/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,7 +5,6 @@ import PromptCard from './PromptCard'
 
 
 const PromptCardList = ({data, handleTagClick})=> {
-  console.log(data)
   return (
     <div className='mt-16 prompt_layout'>
       {data.map((post)=>(
