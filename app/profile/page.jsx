3813f1175a52d92bea19003e35b72d1af1e60dbb
rakhazufar <rakhazufar1@gmail.com>
2023-06-18 23:57:21 +0700

    add profile page, but there is no delete&edit button yet

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,46 @@
 "use client"
 
 import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
-const Profile = () => {
+import Profile from '@components/Profile'
+
+const MyProfile = () => {
+  const {data: session} = useSession()
+  console.log("session", session)
+
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+     const res = await fetch(`/api/users/${session?.user.id}/posts`) 
+     const data = await res.json()
+     console.log("data", data)
+
+     setPosts(data)
+    }
+
+    if (session?.user.id) fetchData()
+  }, [])
+  
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = async () => {
+
+  }
+
   return (
-    <div>Profile</div>
+    <Profile 
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={()=>handleEdit()}
+      handleDelete={()=>handleDelete()}
+    />
   )
 }
 
-export default Profile
+export default MyProfile
